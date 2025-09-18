56046cf8a652daecb97f314a119270224ff4265f
Amit Kumar <a.kumar@supraoracles.com>
2025-09-18 15:43:29 +0530

    remote file download scripts

--- scripts/reset_parallel.sh
+++ scripts/reset_parallel.sh
@@ -5,9 +5,15 @@ REMOTE_USER="ubuntu"
 SSH_KEY_PATH="$HOME/.ssh/automation_registry"    
 
 # List of remote hosts
-REMOTE_HOSTS_VALIDATOR="104.199.143.64 34.146.238.44 34.47.214.94 34.93.67.69 34.143.206.81 34.76.140.195 35.234.142.159"
-REMOTE_HOSTS_RPC="34.14.214.232 34.14.220.53"
+REMOTE_HOSTS_VALIDATOR="35.185.169.13 34.84.78.249 34.14.220.53 34.14.214.232 35.240.228.147 35.241.168.8 35.242.129.120"
+REMOTE_HOSTS_RPC="35.200.202.80 34.47.214.94"
 
+# === Confirmation Prompt ===
+read -p "Do you really want to continue? (y/n): " choice
+if [[ ! "$choice" =~ ^[Yy]$ ]]; then
+  echo "Aborted."
+  exit 1
+fi
 
 # === Step 1: Stop all validators ===
 echo ">>> Stopping all validators..."
@@ -21,7 +27,6 @@ for HOST in $REMOTE_HOSTS_RPC; do
   ssh -i "$SSH_KEY_PATH" "${REMOTE_USER}@${HOST}" "sudo systemctl stop supra-rpc"
 done
 
-
 # === Step 2: Cleanup all validators ===
 echo ">>> Cleaning validator nodes..."
 for HOST in $REMOTE_HOSTS_VALIDATOR; do
@@ -33,7 +38,6 @@ echo ">>> Cleaning RPC nodes..."
 for HOST in $REMOTE_HOSTS_RPC; do
   ssh -i "$SSH_KEY_PATH" "${REMOTE_USER}@${HOST}" "sudo rm -rf rpc_ledger/ rpc_store_pruned/ snapshot/ rpc_archive_pruned/ smr_node_logs/"
 done
-
 
 # === Step 3: Start all validators ===
 echo ">>> Starting all validators..."
